c5b94b94fc8c1c94f3577b9ac34754583686b0cb
JM-rich <richard.maciel1397@gmailcom>
2019-10-11 22:44:40 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/jrmg/jrmgp3/actividad_FormActivity.java
@@ -0,0 +1,18 @@
+package mx.edu.tesoem.jrmg.jrmgp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class actividad_FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad__form);
+
+
+    }
+}
